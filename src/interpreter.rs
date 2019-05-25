5a8b9272e53e663c85c0a01a91a11e9c3e0a7494
Egor Khromov <cheburum@protonmail.com>
2019-05-25 21:58:10 +0300

    Stack and memory

--- src/interpreter.rs
+++ src/interpreter.rs
@@ -2,34 +2,24 @@ use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::fmt;
 
-enum UnaryOperator {
-    IsTrue,
-    IsFalse,
-    Utilised,
-    NotUtilised,
+// Инструкции, помеченные (*) содержат в себе указатель, на инструкцию, начиная с которой
+// надо начать выполнять, чтобы в стэке оказались нужные аргументы
+
+enum Instructions {
+    Generate(usize),  // (*)
+    Advance(usize),   // (*)
+    Terminate(usize), // (*)
+    Print(usize),     // Печатает переменную по адресу usize
+    PrintClock,       // просто печатает время
+    Transfer(usize),  // операндом является номер инструкции для перехода
+    TestVar(usize),   // операндом является номер инструкции для перехода
+    SaveValue(usize), // операндом является номер адрес для записи
+    Push(usize),      // операндом является адрес, откуда брать элемент для вставки в стэк
 }
 
-enum BinaryOperator {
-    Greater,
-    Less,
-    Equal,
-    NotEqual,
-}
-
-enum LogicOperator {
-    Binary(BinaryOperator, usize, usize),
-    Unary(UnaryOperator, usize),
-}
-
-enum Instructions<'a> {
-    Generate(u64),
-    Advance(u64),
-    Terminate(u32),
-    PrintParam(u8),
-    PrintText(&'a str),
-    PrintClock,
-    Transfer(usize),
-    Test(LogicOperator,usize),
+enum EventType {
+    Generate,
+    Advance,
 }
 
 struct Event {
@@ -58,81 +48,84 @@ impl PartialEq for Event {
 
 impl Eq for Event {}
 
-#[derive(Copy, Clone)]
-enum GpssType {
+macro_rules! gpss_type_impl {
+    ($($name:ident($type_of:ty)),+) => {
+
+        #[derive(Clone,Copy)]
+        enum GpssType {
+            $($name($type_of),)+
+        }
+
+        impl PartialEq for GpssType {
+            fn eq(&self, other: & GpssType) -> bool {
+                match self {
+                    $(GpssType::$name(self_val) => match other {
+                        GpssType::$name(other_val) => self_val == other_val,
+                        _ => panic!("Comparing $name and not $name"),
+                    },)
+                    +
+                }
+            }
+        }
+
+        impl PartialOrd for GpssType {
+            fn partial_cmp(&self, other: &GpssType) -> Option<Ordering> {
+                match self {
+                    $(GpssType::$name(self_val) => match other {
+                        GpssType::$name(other_val) => self_val.partial_cmp(other_val),
+                        _ => None,
+                     },)
+                     +
+                }
+            }
+        }
+
+        $(
+        impl From<GpssType> for $type_of {
+            fn from(item: GpssType) -> Self {
+                match &item {
+                    GpssType::$name(value) => *value,
+                    _ => panic!("Cannot convert type to boolean"),
+                }
+            }
+        }
+        )
+        +
+
+        impl From<GpssType> for usize {
+            fn from(item: GpssType) -> Self {
+                match &item {
+                    GpssType::UnsignedInteger(value) => *value as usize,
+                    _ => panic!("Cannot convert type to boolean"),
+                }
+            }
+        }
+
+        impl fmt::Display for GpssType {
+            fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+                match self {
+                    $(GpssType::$name(val) => write!(f, "$name, {}", val),)
+                    +
+                }
+            }
+        }
+        impl GpssType {
+            fn empty() -> GpssType {
+                GpssType::Boolean(false)
+            }
+        }
+    }
+}
+
+gpss_type_impl!(
     Boolean(bool),
     Float(f32),
     Integer(i32),
     Facility(u8),
-}
+    UnsignedInteger(u32)
+);
 
-impl PartialEq for GpssType {
-    fn eq(&self, other: &GpssType) -> bool {
-        match self {
-            GpssType::Boolean(self_val) => match other {
-                GpssType::Boolean(other_val) => self_val == other_val,
-                _ => panic!("Comparing boolean and not boolean"),
-            },
-            GpssType::Float(self_val) => match other {
-                GpssType::Float(other_val) => self_val == other_val,
-                _ => panic!("Comparing float and not float"),
-            },
-            GpssType::Integer(self_val) => match other {
-                GpssType::Integer(other_val) => self_val == other_val,
-                _ => panic!("Comparing integer and not integer"),
-            },
-            _ => panic!("Cannot compare facilities"),
-        }
-    }
-}
-
-impl PartialOrd for GpssType {
-    fn partial_cmp(&self, other: &GpssType) -> Option<Ordering> {
-        match self {
-            GpssType::Boolean(self_val) => match other {
-                GpssType::Boolean(other_val) => self_val.partial_cmp(other_val),
-                _ => None,
-            },
-            GpssType::Float(self_val) => match other {
-                GpssType::Float(other_val) => self_val.partial_cmp(other_val),
-                _ => panic!("Comparing float and not float"),
-            },
-            GpssType::Integer(self_val) => match other {
-                GpssType::Integer(other_val) => self_val.partial_cmp(other_val),
-                _ => panic!("Comparing integer and not integer"),
-            },
-            _ => panic!("Cannot compare facilities"),
-        }
-    }
-}
-
-impl From<GpssType> for bool {
-    fn from(item: GpssType) -> Self {
-        match item {
-            GpssType::Boolean(value) => value,
-            _ => panic!("Cannot convert type to boolean"),
-        }
-    }
-}
-
-impl fmt::Display for GpssType {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            GpssType::Boolean(b) => write!(f, "Boolean, {}", b),
-            GpssType::Float(fl) => write!(f, "Float, {}", fl),
-            GpssType::Integer(int) => write!(f, "Int, {}", int),
-            GpssType::Facility(count) => write!(f, "Facility with: {} items", count),
-        }
-    }
-}
-
-impl GpssType {
-    fn empty() -> GpssType {
-        GpssType::Boolean(false)
-    }
-}
-
-#[derive(Copy, Clone)]
+#[derive(Clone)]
 struct Transact {
     params: [GpssType; 16],
 }
@@ -145,41 +138,58 @@ impl Transact {
     }
 }
 
-pub struct Interpreter<'a> {
-    instructions: Vec<Instructions<'a>>,
+pub struct Interpreter {
+    instructions: Vec<Instructions>,
     current_instruction: usize,
     current_transact: Option<Transact>,
     start_entities: u32,
     current_time: u64,
     events: BinaryHeap<Event>,
-    variables: Vec<GpssType>,
+    memory: Vec<GpssType>,
+    stack: Vec<GpssType>,
 }
 
-impl<'a> Interpreter<'a> {
-    pub fn build_test_interpreter() -> Interpreter<'a> {
+impl Interpreter {
+    fn build_interpreter(instructions: Vec<Instructions>, memory: Vec<GpssType>) -> Interpreter {
         Interpreter {
-            instructions: vec![
-                //Instructions::PRINT_CLOCK,
-                Instructions::Generate(2),
-                //Instructions::PRINT_CLOCK,
-                //Instructions::PRINT_PARAM(0),
-                Instructions::Transfer(3),
-                Instructions::Advance(1),
-                //Instructions::PRINT_CLOCK,
-                //Instructions::PRINT_TEXT("Object destroyed".into()),
-                Instructions::Terminate(1),
-            ],
+            instructions,
             current_instruction: 0,
             current_transact: None,
             start_entities: 15,
             current_time: 0,
             events: BinaryHeap::new(),
-            variables: Vec::new(),
+            memory,
+            stack: Vec::new(),
         }
     }
 
+    pub fn build_test_interpreter() -> Interpreter {
+        Self::build_interpreter(
+            vec![
+                Instructions::Push(1),      // Какой обьект сохранить (#1)
+                Instructions::SaveValue(0), // Вызов инструкции для сохранения значения
+                Instructions::Push(0),      // Generate возмет время генерации из ячейки #2
+                Instructions::Generate(2),  // Generate в следующий раз вернется на 2-ую иструкцию
+                Instructions::Transfer(5),
+                Instructions::Push(2),
+                Instructions::Advance(5),
+                Instructions::PrintClock,
+                Instructions::Push(3),
+                Instructions::Terminate(8),
+                Instructions::Push(3),
+                Instructions::Terminate(10),
+            ],
+            vec![
+                GpssType::UnsignedInteger(0),
+                GpssType::Float(0.01),
+                GpssType::Float(0.02),
+                GpssType::UnsignedInteger(1)
+            ],
+        )
+    }
+
     fn fraction_time_to_int(t: f32) -> u64 {
-        (t * 100.0) as u64
+        (t * 1000.0) as u64
     }
 
     fn perform_closest(&mut self) {
@@ -190,8 +200,21 @@ impl<'a> Interpreter<'a> {
                 self.current_time = nearest_event.wake_time;
                 info!("Woke up at {}", self.current_time);
                 self.current_transact = nearest_event.transact;
+
                 match self.instructions[nearest_event.instruction_id] {
-                    Instructions::Generate(time) => {
+                    Instructions::Generate(begin) | Instructions::Advance(begin) => {
+                        self.current_instruction = begin;
+                        while self.current_instruction < nearest_event.instruction_id {
+                            self.process_instruction();
+                            self.current_instruction += 1;
+                        }
+                    }
+                    _ => return,
+                };
+
+                match self.instructions[nearest_event.instruction_id] {
+                    Instructions::Generate(_) => {
+                        let time = self.stack_pop_time();
                         info!("DOING GENERATE");
                         let mut new_transact = Transact::empty();
                         new_transact.params[0] = GpssType::Integer(rand::random::<i32>());
@@ -232,90 +255,92 @@ impl<'a> Interpreter<'a> {
         }
     }
 
+    fn stack_pop(&mut self) -> GpssType {
+        self.stack.pop().expect("Stack is empty, but trying to pop")
+    }
+
+    fn stack_pop_time(&mut self) -> u64 {
+        Self::fraction_time_to_int(self.stack_pop().into())
+    }
+
+    fn process_instruction(&mut self) {
+        match self.instructions[self.current_instruction] {
+            //Блоки, требущие подождать. Создаем для них событие в будущем
+            Instructions::Generate(_) => {
+                let time = self.stack_pop_time();
+                self.create_event(self.current_instruction, self.current_time + time, None);
+                // После того, как создали новое событие
+                // ищем и исполняем ближайшее
+                self.perform_closest();
+            }
+            Instructions::Advance(_) => {
+                let time = self.stack_pop_time();
+                info!("Wake time for ADVANCE {}", self.current_time + time);
+                self.create_event(
+                    self.current_instruction,
+                    self.current_time + time,
+                    self.current_transact.clone(),
+                );
+                self.perform_closest();
+            }
+            //Блоки, не требующие подождать
+            Instructions::Terminate(_) => {
+                let count: u32 = self.stack_pop().into();
+                info!("TERMINATE {}", count);
+                self.start_entities -= count;
+                self.current_transact = None;
+                if self.events.len() > 0 {
+                    self.perform_closest();
+                } else {
+                    info!("STOP");
+                    self.current_instruction += 1;
+                }
+            }
+            Instructions::Print(var_id) => {
+                println!("{}", self.memory[var_id]);
+                self.current_instruction += 1;
+            }
+            Instructions::PrintClock => {
+                println!("Clock {}", self.current_time);
+                self.current_instruction += 1;
+            }
+            Instructions::Transfer(instruction_id) => {
+                info!(
+                    "TRANSFER FROM {} TO {}",
+                    self.current_instruction, instruction_id
+                );
+                self.current_instruction = instruction_id;
+            }
+            Instructions::TestVar(else_goto) => {
+                let cond_result = self.stack_pop().into();
+                if cond_result {
+                    self.current_instruction += 1;
+                } else {
+                    self.current_instruction = else_goto;
+                }
+            }
+            Instructions::SaveValue(var_id) => {
+                let object = self.stack_pop();
+                if self.memory.len() > var_id {
+                    self.memory[var_id] = object.clone();
+                } else if self.memory.len() == var_id {
+                    self.memory.push(object.clone());
+                } else {
+                    panic!("Cannot access variable {}", var_id);
+                }
+                self.current_instruction += 1;
+            }
+            Instructions::Push(var_id) => {
+                info!("Push: {}", self.memory[var_id]);
+                self.stack.push(self.memory[var_id]);
+                self.current_instruction += 1;
+            }
+        };
+    }
+
     pub fn process(&mut self) {
         while self.start_entities > 0 && self.current_instruction < self.instructions.len() {
-            match &self.instructions[self.current_instruction] {
-                //Блоки, требущие подождать. Создаем для них событие в будущем
-                Instructions::Generate(time) => {
-                    self.create_event(self.current_instruction, self.current_time + time, None);
-                    // После того, как создали новое событие
-                    // ищем и исполняем ближайшее
-                    self.perform_closest();
-                }
-                Instructions::Advance(time) => {
-                    info!("Wake time for ADVANCE {}", self.current_time + time);
-                    self.create_event(
-                        self.current_instruction,
-                        self.current_time + time,
-                        self.current_transact.clone(),
-                    );
-                    self.perform_closest();
-                }
-                //Блоки, не требующие подождать
-                Instructions::Terminate(count) => {
-                    info!("TERMINATE {}", count);
-                    self.start_entities -= count;
-                    self.current_transact = None;
-                    if self.events.len() > 0 {
-                        self.perform_closest();
-                    } else {
-                        info!("STOP");
-                        self.current_instruction += 1;
-                    }
-                }
-                Instructions::PrintParam(param_id) => {
-                    println!(
-                        "{}",
-                        self.current_transact.unwrap().params[*param_id as usize]
-                    );
-                    self.current_instruction += 1;
-                }
-                Instructions::PrintText(param) => {
-                    println!("{}", param);
-                    self.current_instruction += 1;
-                }
-                Instructions::PrintClock => {
-                    println!("Clock {}", self.current_time);
-                    self.current_instruction += 1;
-                }
-                Instructions::Transfer(instruction_id) => {
-                    info!(
-                        "TRANSFER FROM {} TO {}",
-                        self.current_instruction, instruction_id
-                    );
-                    self.current_instruction = *instruction_id;
-                }
-                Instructions::Test(operator, else_goto) => {
-                    let cond_result: bool = match operator {
-                        LogicOperator::Binary(bin_operator, a, b) => match bin_operator {
-                            BinaryOperator::Equal => self.variables[*a] == self.variables[*b],
-                            BinaryOperator::Greater => self.variables[*a] > self.variables[*b],
-                            BinaryOperator::Less => self.variables[*a] < self.variables[*b],
-                            BinaryOperator::NotEqual => self.variables[*a] != self.variables[*b],
-                        },
-                        LogicOperator::Unary(un_operator, a) => match un_operator {
-                            UnaryOperator::IsFalse => {
-                                let res: bool = self.variables[*a].into();
-                                !res
-                            }
-                            UnaryOperator::IsTrue => self.variables[*a].into(),
-                            UnaryOperator::NotUtilised => {
-                                !Self::is_facility_utilised(self.variables[*a])
-                                    .expect("Not a facility")
-                            }
-                            UnaryOperator::Utilised => {
-                                Self::is_facility_utilised(self.variables[*a])
-                                    .expect("Not a facility")
-                            }
-                        },
-                    };
-                    if cond_result{
-                        self.current_instruction += 1;
-                    }else{
-                        self.current_instruction = *else_goto;
-                    }
-                }
-            };
+            self.process_instruction();
         }
     }
 }
